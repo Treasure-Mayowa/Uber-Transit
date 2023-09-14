aee7e73f2e359dc295120e9b40cf01f9a0044ace
Treasure Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-09-14 21:28:43 +0000

    Create index view

--- core/transit/views.py
+++ core/transit/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Transit")
