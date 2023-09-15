4d16309737b3bde3bb9f10a542c215b5a1f88e72
Treasure Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-09-15 20:44:21 +0000

    Add user authentication

--- core/transit/urls.py
+++ core/transit/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("logout", views.logout_view, name="logout"),
+    path("login", views.login_view, name="login"),
+    path("register", views.register, name="register"),
 ]
--- core/transit/views.py
+++ core/transit/views.py
@@ -11,3 +11,58 @@ from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return HttpResponse("Transit")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "news_suite/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "news_suite/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("login"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "news_suite/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "news_suite/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "news_suite/register.html")
+
+        
+def custom_404(request):
+    return render(request, "news_suite/404.html", status=404)
