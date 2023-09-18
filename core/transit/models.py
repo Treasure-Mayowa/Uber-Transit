dd8189b67f6a3dd79ba4d9e98e41bfc352b7460d
Treasure Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-09-18 09:57:14 +0000

    Update models fields

--- core/transit/models.py
+++ core/transit/models.py
@@ -2,14 +2,20 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class User(AbstractUser):
-    pass
-    
-class Driver(models.Model):
-    pass
-
 class Location(models.Model):
-    pass
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return f"{self.latitude}° lat and {self.longitude}° long" 
+
+
+class User(AbstractUser):
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="location")
+    driver = models.BooleanField(default=False)
+    
 
 class Transit(models.Model):
-    pass
+    name = models.CharField(max_length=150)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="routes")
+    transit_type = models.CharField(max_length=50)
