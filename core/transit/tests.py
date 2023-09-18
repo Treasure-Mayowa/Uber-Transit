671398d63378bb8222bc22f9ad58ff6611bc2172
Treasure Mayowa <69307446+Treasure-Mayowa@users.noreply.github.com>
2023-09-18 12:36:00 +0000

    Setup unit tests

--- core/transit/tests.py
+++ core/transit/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class TestTransit(TestCase):
+
+    def test_index(self):
+        c = Client()
+
+        response = c.get("")
+
+        self.assertEqual(response.status_code, 200)
