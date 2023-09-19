bb07484a65206de28c6c3a6e7c0e41c6e0c5d588
Treasure Mayowa <treasuremayowa@outlook.com>
2023-09-19 14:44:32 +0100

    Minor changes

--- core/transit/views.py
+++ core/transit/views.py
@@ -7,6 +7,8 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import *
+
 # Create your views here.
 
 def index(request):
