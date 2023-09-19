b79466f9bd65555c7c9dd7c46e844e735df7fea1
Treasure Mayowa <treasuremayowa@outlook.com>
2023-09-19 14:08:02 +0100

    Create dashboard url pattern

--- core/transit/urls.py
+++ core/transit/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("login", views.login_view, name="login"),
     path("register", views.register, name="register"),
+    path("dashboard", views.dashboard, name="dashboard")
 ]
