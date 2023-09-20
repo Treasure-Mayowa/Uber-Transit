fb5b7c3c0b9ed620d801cfa60267c67dfa00df34
Treasure Mayowa <treasuremayowa@outlook.com>
2023-09-21 00:18:05 +0100

    Include test for dashboards

--- core/transit/tests.py
+++ core/transit/tests.py
@@ -10,3 +10,15 @@ class TestTransit(TestCase):
         response = c.get("")
 
         self.assertEqual(response.status_code, 200)
+    
+    def test_dashboard(self):
+        c = Client()
+
+        response = c.get("dashboard")
+        self.assertEqual(response.status_code, 404)
+
+    def test_driver_dashboard(self):
+        c = Client()
+
+        response = c.get("driver/dashboard")
+        self.assertEqual(response.status_code, 404)
