ada042183d0a7a2e1f87a670d07f843a7d6fb883
Treasure Mayowa <treasuremayowa@outlook.com>
2023-09-20 14:42:22 +0100

    Alter location field in User model

--- core/transit/models.py
+++ core/transit/models.py
@@ -1,20 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
 class Location(models.Model):
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.latitude}° lat and {self.longitude}° long" 
 
 
 class User(AbstractUser):
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="location", default="null")
-    driver = models.BooleanField(default=False)
-    seats = models.IntegerField(default=0)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name="location", default=None, null=True)
     
+class Driver(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    available_seats = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username} at {self.location}"
+
+class Ride(models.Model):
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name="rides")
+    destination = models.ForeignKey(Location, on_delete=models.CASCADE)
+    departure_time = models.DateTimeField(auto_now=True)
+    available_seats = models.IntegerField(default=0)
+    passengers = models.ManyToManyField(User, related_name="rides_taken", blank=True)
 
 class Transit(models.Model):
     name = models.CharField(max_length=150)
